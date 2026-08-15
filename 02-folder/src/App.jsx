456d622383e3607da7eea3d472fe8134606adbe2
sakshyam08 <sakshyamkoirala777@gmail.com>
2026-08-15 13:19:44 +0545

    add React components project

---
+++ 02-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/nav.jsx";
+import Card from "./components/card.jsx";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <Card />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ 02-folder/src/components/card.jsx
@@ -0,0 +1,16 @@
+function Card() {
+  return (
+    <div className="card">
+      <h2>About Me</h2>
+
+      <p>
+        Hi, I'm Sakshyam. I'm a B.Sc. CSIT student and a web development
+        enthusiast. I enjoy building websites and learning new technologies.
+      </p>
+
+      <button>Read More</button>
+    </div>
+  );
+}
+
+export default Card;
