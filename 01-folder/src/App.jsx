05326c0a5b025110b95f2a0a9e790f7d0aad8c1f
sakshyam08 <sakshyamkoirala777@gmail.com>
2026-08-15 12:24:14 +0545

    react first folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <>
+   <div className="dad">
+    
+    </div>
+     <div className="chaha">
+    
+    </div></>
+    
+  )
+}
+
+export default App
